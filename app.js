25c0924d612271688d93508ebff5cfb043b059ca
raul-1201 <mironraul87@gmail.com>
2021-06-04 02:20:18 +0300

    tema

--- app.js
+++ app.js
@@ -30,155 +30,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
 
 
-var utilizatori;
-fs.readFile('utilizatori.json', (err, data) => {
-    if (err) throw err;
-    utilizatori = JSON.parse(data);
-    
-});
 
-var con = mysql.createConnection({
-	host: "localhost",
-	user: "test",
-	password: "test1234"
-  });
-
-
-var con1=mysql.createConnection({
-	host: "localhost",
-	user: "test",
-	password: "test1234",
-	database: "Cumparaturi"
-  });
 app.get('/', (req, res) => {
 	
 
-	res.render('index',{log:req.cookies['cookieLogin']});
+	res.render('index');
 	
 
 });
 
-app.get('/creare-bd', (req, res) => {
+app.get('/ponturi-pariuri', (req, res) => res.render('ponturi-pariuri'));
+
+app.get('/joc', (req, res) => {
 	
-	con.query("CREATE DATABASE IF NOT EXISTS Cumparaturi", function (err, result) {
-		
-		console.log("Database created");
-	  }); 
-
-	  sql="CREATE TABLE IF NOT EXISTS Cumparaturi.Produse(idProdus INT primary key, nume VARCHAR(255), pret INT);"; 
-	  con.query(sql,function(err,result){ 
-		  console.log("Table created!");
-	  });
-	  res.redirect('/')
-}); 
-
-app.get('/stergere-bd', (req, res) => {
-	
-	var sql = "DROP TABLE Cumparaturi.Produse";
-  	con.query(sql, function (err, result) {
-    console.log("Table deleted");
-  });
-	  res.redirect('/')
+	res.render('joc');
 });
 
-app.get('/inserare-bd', (req, res) => {
-	var valori=[ 
-		[1,'Caiet',5], 
-		[2,'Dosar',7], 
-		[3,'Carte',15]	 
-];
-	var sql = "INSERT INTO Cumparaturi.Produse (idProdus, nume, pret) VALUES ?";
-  	con.query(sql,[valori], function (err, result) { 
-	console.log("Records inserted");
-});
-	  res.redirect('/')
-}); 
-
-
-app.get('/autentificare', (req, res) => res.render('autentificare',{cookieErr:req.cookies['cookieErr']}));
-app.post('/verificare-autentificare', (req, res) => {
-	console.log(req.body);
-	//console.log(req.body["uname"]);
-	if(req.body["uname"]==utilizatori[0].uname && req.body["psw"]==utilizatori[0].psw )
-	{
-		res.cookie('cookieLogin','Bine ai venit, '+ req.body['uname']+'!',{maxAge:600000,httpOnly:true});
-		
-		if(req.cookies['uname']!=null)
-		{
-			res.clearCookie('uname');
-		}
-		res.redirect('/');
-		
-	} 
-	else if(req.body["uname"]==utilizatori[1].uname && req.body["psw"]==utilizatori[1].psw ) 
-	{
-
-		res.cookie('cookieLogin','Bine ai venit, '+ req.body['uname']+'!',{maxAge:600000,httpOnly:true});
-		
-		if(req.cookies['uname']!=null)
-		{
-			res.clearCookie('uname');
-		}
-		res.redirect('/');
-		
-	}
-	  	
-		
-	else
-	{
-		res.cookie('cookieErr','Utilizatorul sau parola sunt gresite',{maxAge:1000,httpOnly:true});
-		res.cookie('uname', null);
-		res.redirect('/autentificare');
-		
-		res.end();
-	}
-	
-});
-
-
-
-var intrebari;
-fs.readFile('intrebari.json', (err, data) => {
-    if (err) throw err;
-    intrebari = JSON.parse(data);
-    
-});
-
-
-// la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
-app.get('/chestionar', (req, res) => {
-	
-	res.render('chestionar', {intrebari});
-});
-
-app.post('/rezultat-chestionar', (req, res) => {
-		//console.log(req.body);
-		//res.send("formular: " + JSON.stringify(req.body));
-		
-		var i;
-		var raspunsCorect=0 ;
-		for(i=0;i<intrebari.length;i++)
-		{
-			
-			
-			if(intrebari[i].corect==req.body["var"+(i+1)])
-			{
-
-				raspunsCorect++;
-				
-			}
-			
-		}
-		
-		res.render('rezultat-chestionar',{intrebari,raspunsCorect })
-});
-
-
-app.get('/delogare', (req, res) => {
-	if(req.cookies['cookieLogin']!=null)
-	{
-		res.clearCookie('cookieLogin');
-		res.redirect('/');
-	}
-});
 app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:`));
